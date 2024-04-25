12e556dcb4ecd6aba0f557a5a29cc67185036768
gaeun7 <162283154+gaeun7@users.noreply.github.com>
2024-04-25 12:01:18 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/README.md
+++ 프로그래머스/0/120829. 각도기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 각도기 - 120829 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120829?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120829) 
 
 ### 성능 요약
 
-메모리: 72.8 MB, 시간: 0.01 ms
+메모리: 81.8 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 25일 11:57:53
+2024년 04월 25일 12:01:18
 
 ### 문제 설명
 
--- 프로그래머스/0/120829. 각도기/각도기.java
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -1,13 +1,15 @@
 class Solution {
     public int solution(int angle) {
+        int answer = 0;
+        
        if (0 < angle && angle < 90){
-           return 1;
+           answer = 1;
        } else if (angle == 90){
-           return 2;
+           answer = 2;
        } else if (90 < angle && angle < 180) {
-           return 3;
+           answer = 3;
        } else if(angle == 180){
-           return 4;
-       } return 0;
+           answer = 4;
+       } return answer;
     }
 }
