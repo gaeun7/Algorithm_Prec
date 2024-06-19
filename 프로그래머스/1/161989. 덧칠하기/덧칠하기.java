8ccdfff43f3c1dc776c95d16a687d729e110b501
GaEun <162283154+gaeun7@users.noreply.github.com>
2024-06-19 17:23:18 +0900

    [level 1] Title: 덧칠하기, Time: 1.22 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int roller = section[0];
+        int cnt = 1;
+        for(int i = 1; i < section.length; i++) {
+            if(roller + m - 1 < section[i]) {
+                cnt++;
+                roller = section[i];
+            }
+        }
+        return cnt;
+    }
+}
